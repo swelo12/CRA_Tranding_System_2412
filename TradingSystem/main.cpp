7d1a1653e3223f2be02432c0d513f47d203b0619
wj <swelo12@naver.com>
2025-07-04 09:55:40 +0900

    [test] auto trading system

--- TradingSystem/main.cpp
+++ TradingSystem/main.cpp
@@ -6,18 +6,21 @@ using namespace testing;
 
 class TraingFixture {
 public:
-	StockerBrockerDriverInterface sbdInterface;
+	StockerBrockerDriverInterface mockInterface;
 	std::string ID = "1234";
 	std::string SUCCESS_PASSWORD = "1234";
-	std::string stockCode = "code";
+	std::string STOCK_CODE = "code";
+
+	AutoTradingSystem* autoTradingSystem; // need to  inheritance
 
 private:
 
 };
 
+/* 1. StockerBrockerDriverInterface Mock Test*/
 TEST_F(TraingFixture, TEST_LOGIN)
 {
-	sbdInterface.login(ID, SUCCESS_PASSWORD);
+	mockInterface.login(ID, SUCCESS_PASSWORD);
 
 	EXPECT_CALL(StockerBrockerDriverInterface, login(_,_))
 		.call(1);
@@ -28,7 +31,7 @@ TEST_F(TraingFixture, TEST_BUY)
 	int price = 100;
 	int num = 10;
 
-	sbdInterface.buy(stockCode, price, num);
+	mockInterface.buy(STOCK_CODE, price, num);
 
 	EXPECT_CALL(StockerBrockerDriverInterface, buy(_,_, _))
 		.call(1);
@@ -39,7 +42,7 @@ TEST_F(TraingFixture, TEST_SELL)
 	int price = 100;
 	int num = 10;
 
-	sbdInterface.sell(stockCode, price, num);
+	mockInterface.sell(STOCK_CODE, price, num);
 
 	EXPECT_CALL(StockerBrockerDriverInterface, sell(_, _, _))
 		.call(1);
@@ -48,10 +51,73 @@ TEST_F(TraingFixture, TEST_SELL)
 TEST_F(TraingFixture, TEST_GET_PRICE)
 {
 	int except = 100;
-	int actual = sbdInterface.getPrice(stockCode);
+	int actual = mockInterface.getPrice(STOCK_CODE);
 
 	EXPECT_EQ(except, actual);
 }
+
+
+/* 2. Auto trading Test*/
+TEST_F(TraingFixture, TEST_buyNiceTiming__Buy_success)
+{
+	autoTradingSystem = new AutoTradingSystem(mockInterface); // inherit mock.
+	std::vector<int> price = { 12,20,30 }
+
+	EXPECT_CALL(mockInterface, getPrice(_))
+		.WillOnce(Return(price[0]))
+		.WillOnce(Return(price[1]))
+		.WillOnce(Return(price[2]));
+
+	bool success = autoTradingSystem->buyNiceTiming(STOCK_CODE, price[2]);
+
+	EXPECT_EQ(true, success);
+}
+
+TEST_F(TraingFixture, TEST_buyNiceTiming__Buy_fail)
+{
+	autoTradingSystem = new AutoTradingSystem(mockInterface); // inherit mock.
+	std::vector<int> price = { 40,20,30 }
+
+		EXPECT_CALL(mockInterface, getPrice(_))
+		.WillOnce(Return(price[0]))
+		.WillOnce(Return(price[1]))
+		.WillOnce(Return(price[2]));
+
+	bool success = autoTradingSystem->buyNiceTiming(STOCK_CODE, price[2]);
+
+	EXPECT_EQ(false, success);
+}
+
+TEST_F(TraingFixture, TEST_sellNiceTiming__Buy_success)
+{
+	autoTradingSystem = new AutoTradingSystem(mockInterface); // inherit mock.
+	std::vector<int> price = { 12,20,30 }
+
+		EXPECT_CALL(mockInterface, getPrice(_))
+		.WillOnce(Return(price[0]))
+		.WillOnce(Return(price[1]))
+		.WillOnce(Return(price[2]));
+
+	bool success = autoTradingSystem->sellNiceTiming(STOCK_CODE, price[2]);
+
+	EXPECT_EQ(true, success);
+}
+
+TEST_F(TraingFixture, TEST_sellNiceTiming__Buy_fail)
+{
+	autoTradingSystem = new AutoTradingSystem(mockInterface); // inherit mock.
+	std::vector<int> price = { 40,20,30 }
+
+		EXPECT_CALL(mockInterface, getPrice(_))
+		.WillOnce(Return(price[0]))
+		.WillOnce(Return(price[1]))
+		.WillOnce(Return(price[2]));
+
+	bool success = autoTradingSystem->sellNiceTiming(STOCK_CODE, price[2]);
+
+	EXPECT_EQ(false, success);
+}
+
 
 int main() {
 	::testing::InitGoogleMock();
