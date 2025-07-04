7a3ba0403d353c0c90a34bba1c799e0ae6d1ade8
jongmin.lee <znargon@gmail.com>
2025-07-04 12:54:52 +0900

    DeviceDriver: function rename.

--- TradingSystem/nemo_driver.cpp
+++ TradingSystem/nemo_driver.cpp
@@ -5,22 +5,22 @@ class NemoDriver : public StockBrokerDriver {
 public:
 
 	void login(std::string ID, std::string password) {
-		nemoAPI->certification(ID, password);
+		nemoAPI.certification(ID, password);
 		std::cout << ID << " [NemoDriver] login success\n";
 	}
 
 	void buy(std::string stockCode, int count, int price) {
-		nemoAPI->purchasingStock(stockCode, count, price);
+		nemoAPI.purchasingStock(stockCode, count, price);
 		std::cout << stockCode << " : [NemoDriver] Buy stock ( " << price << " * " << count << ")\n";
 	}
 
 	void sell(std::string stockCode, int count, int price) {
-		nemoAPI->sellingStock(stockCode, count, price);
+		nemoAPI.sellingStock(stockCode, count, price);
 		std::cout << stockCode << " : [NemoDriver] Sell stock ( " << price << " * " << count << ")\n";
 	}
 
-	int currentPrice(std::string stockCode) {
-		int stockPrice = nemoAPI->getMarketPrice(stockCode, 0);
+	int getPrice(std::string stockCode) {
+		int stockPrice = nemoAPI.getMarketPrice(stockCode, 0);
 		if (stockPrice < 0) {
 			std::cout << "[NemoDriver] Abnormal price. price set to 0" << "\n";
 			stockPrice = 0;
@@ -28,5 +28,5 @@ public:
 		return stockPrice;
 	}
 private:
-	NemoAPI* nemoAPI;
+	NemoAPI nemoAPI;
 };
